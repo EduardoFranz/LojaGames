8990d3f939efc30eb9718129b7dc563468069555
Eduardo Franz <eduardo.franz77@gmail.com>
2022-07-10 19:32:42 -0300

    -add controller e repository

---
+++ LojaGames/src/main/java/com/generation/LojaGames/controller/ProdutoController.java
@@ -0,0 +1,35 @@
+package com.generation.LojaGames.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.LojaGames.model.Produto;
+import com.generation.LojaGames.repository.CategoriaRepository;
+import com.generation.LojaGames.repository.ProdutoRepository;
+
+
+
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll (){
+		return ResponseEntity.ok(produtoRepository.findAll());
+	}
+}
---
+++ LojaGames/src/main/java/com/generation/LojaGames/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.LojaGames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.LojaGames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+
+}
